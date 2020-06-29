9c9eafd3a718a10b85cfd105f9bb73ef04491775
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-06-30 00:31:48 +0900

    WeDaily Controller
    /move2 : 메인페이지 이동하면서 영화순위를 가져오는 Controller 역할
                   영화진흥위원회 제공 API를 사용했으며
                     Date 객체를 사용하여 전날기준 영화순위를 가져옵니다.
    
    /moveselect : 영화검색을 할때 사용하는 Controller
                            NAVER에서 제공하는 검색 API를 사용
                            최대 10개까지의 동시검색이 가능합니다.

---
+++ java/WeDailyController.java
@@ -0,0 +1,243 @@
+package collabo.wato.springboot.web.WeDaily;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.parser.ParseException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import collabo.wato.springboot.web.WeDaily.vo.WeDailyVO;
+
+@Controller
+public class WeDailyController {
+	
+	
+	public static StringBuilder sb;//
+	  
+	    static String getString(String input, String data) // API에서 필요한 문자 자르기.
+	    {
+	        String[] dataSplit = data.split("{" + input + "}");
+	        String[] dataSplit2 = dataSplit[1].split("\"" + input + "\"");
+	        return dataSplit2[0];
+	    }
+
+	
+		@RequestMapping("/moveselect")
+	    public String mone2(HttpServletRequest request, HttpServletResponse response) throws ParseException {
+	    	String monetitle = request.getParameter("search");
+	    	
+	    	String clientId = "aEcAqL0ijo1Ekkj9Mfrr";
+	        String clientSecret = "JsY0vYGSGy";
+	        int display = 10;
+	        
+	        WeDailyVO vo = new WeDailyVO();
+	 
+	        List<WeDailyVO> titleArr = new ArrayList<WeDailyVO>();
+	       
+	        try {
+	            String text = URLEncoder.encode(monetitle, "utf-8");
+	            String apiURL = "https://openapi.naver.com/v1/search/movie.json?query=" + text + "&display=" + display + "&";
+	            URL url = new URL(apiURL);
+	            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	            con.setRequestMethod("GET");
+	            con.setRequestProperty("X-Naver-Client-Id", clientId);
+	            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+	            int responseCode = con.getResponseCode();
+	            BufferedReader br;
+	            if (responseCode == 200) {
+	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	            } else {
+	                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	            }
+	            sb = new StringBuilder();
+	            String line;
+	 
+	            while ((line = br.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+	 
+	            br.close();
+	            con.disconnect();
+	            //System.out.println(sb); 
+	            String data = sb.toString();
+	            String[] array;
+	            array = data.split("\"");
+	            String[] title = new String[display];
+	            String[] link = new String[display];
+	            String[] image = new String[display];
+	            String[] category = new String[display];
+	            String[] description = new String[display];
+	            String[] telephone = new String[display];
+	            String[] address = new String[display];
+	            String[] mapx = new String[display];
+	            String[] mapy = new String[display];
+	            String[] actor = new String[display];
+	            
+	            int k = 0;
+	            for (int i = 0; i < array.length; i++) {
+	            
+	                if (array[i].equals("title"))
+	                    title[k] = array[i + 2];
+	                if (array[i].equals("link"))
+	                    link[k] = array[i + 2];
+	                if (array[i].equals("category"))
+	                    category[k] = array[i + 2];
+	                if (array[i].equals("description"))
+	                    description[k] = array[i + 2];
+	                if (array[i].equals("telephone"))
+	                    telephone[k] = array[i + 2];
+	                if (array[i].equals("address"))
+	                    address[k] = array[i + 2];
+	                if (array[i].equals("mapx"))
+	                    mapx[k] = array[i + 2];
+	                if (array[i].equals("image")) {
+	                	image[k] = array[i + 2];
+	                }if(array[i].equals("actor")) {
+	                	actor[k] = array[i + 2];
+	                	k++;
+	                }	         
+	            }
+	            
+	            //System.out.println("sb >> " + sb);
+	            
+	            for(int j = 0; j < title.length; ) {
+	            	
+	            	System.out.println("title >> " +title[j]);
+	            	System.out.println("image >> " +image[j]);
+	            	System.out.println("actor >> " +actor[j]);
+	            	       
+	            	vo.setTitle(title[j]);
+	            	vo.setActor(actor[j]);
+	            	vo.setImage(image[j]);
+	            	titleArr.add(vo);
+	            	vo = new WeDailyVO();
+	            	
+	            	j++;
+	            }
+	          
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }
+	        
+	        request.setAttribute("titleArr",titleArr);
+			return "/main/WeDaily/moveResult";
+		}
+	
+
+
+		// 페이지 이동
+	    @RequestMapping("/move2")
+	    public String move2(HttpServletRequest request, HttpServletResponse response)throws Exception {
+	    	 
+	        int display = 10;
+	        
+	        WeDailyVO vo = new WeDailyVO();
+
+	        List<WeDailyVO> rankArr = new ArrayList<WeDailyVO>();        
+	        Date today = new Date();     
+	        SimpleDateFormat date = new SimpleDateFormat("yyyyMMdd");
+	        
+	        System.out.println("Date: "+date.format(today));
+
+	        String time = date.format(today);
+	        int Yesterday = Integer.parseInt(time) - 1;  
+	    
+	        try {
+	            //String text = URLEncoder.encode(monetitle, "utf-8");
+	            String apiURL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=bb67dec08a2eae9eec1bdcc1328fcbce&targetDt=" + Yesterday;
+	            URL url = new URL(apiURL);
+	            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	            con.setRequestMethod("GET");
+	           
+	            int responseCode = con.getResponseCode();
+	            BufferedReader br;
+	            if (responseCode == 200) {
+	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	            } else {
+	                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	            }
+	            sb = new StringBuilder();
+	            String line;
+
+	            while ((line = br.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+
+	            br.close();
+	            con.disconnect();
+	            //System.out.println(sb); 
+	            String data = sb.toString();
+	            String[] array;
+	            array = data.split("\"");
+	            String[] rank = new String[display];
+	            String[] movieNm = new String[display];
+	            String[] openDt = new String[display];	         
+	            String[] audiCnt = new String[display];
+	            String[] audiAcc = new String[display];
+	            
+	            String[] category = new String[display];
+	            String[] address = new String[display];
+	            String[] mapx = new String[display];
+	            String[] image = new String[display];
+	            String[] actor = new String[display];
+	            
+	            int k = 0;
+	            for (int i = 0; i < array.length; i++) {
+	           
+	            	 	if (array[i].equals("rank"))
+	            		 rank[k] = array[i + 2];
+		                if (array[i].equals("movieNm"))
+		                	movieNm[k] = array[i + 2];		              
+		                if (array[i].equals("openDt"))
+		                	openDt[k] = array[i + 2];
+		                if (array[i].equals("audiCnt"))
+		                	audiCnt[k] = array[i + 2];
+		                if (array[i].equals("audiAcc")) {
+		                	audiAcc[k] = array[i + 2];
+		                	k++;
+		                }
+		                
+	            }
+	            System.out.println("여기까지옴?");
+				for(int y = 0; y < rank.length; ) {
+					            	
+	            	System.out.println("rank >> " +rank[y]);
+	            	System.out.println("movieNm >> " +movieNm[y]);
+	            	System.out.println("openDt >> " +openDt[y]);
+	            	       
+	            	vo.setRank(rank[y]);
+	            	vo.setMovieNm(movieNm[y]);
+	            	vo.setOpenDt(openDt[y]);
+	            	vo.setAudiCnt(audiCnt[y]);
+	            	vo.setAudiAcc(audiAcc[y]);
+	            	rankArr.add(vo);
+	            	vo = new WeDailyVO();
+	            
+	            	y++;
+	            }
+	                   
+	            System.out.println("sb >> " + sb);	         
+	          
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }
+	        	request.setAttribute("rankArr",rankArr);
+	    	return "/main/WeDaily/WeDailyMain";
+	    }
+		
+		
+		
+	
+
+}
