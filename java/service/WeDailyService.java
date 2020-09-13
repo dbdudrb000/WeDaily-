d426c95f3fdf777591e7ae9030c0480d75e787bb
dbdudrb000 <65213623+dbdudrb000@users.noreply.github.com>
2020-09-13 20:26:01 +0900

    WeDaily Service File update

--- java/service/WeDailyService.java
+++ java/service/WeDailyService.java
@@ -1,7 +1,6 @@
 package collabo.wato.springboot.web.WeDaily.service;
 
 import java.util.List;
-
 import collabo.wato.springboot.web.WeDaily.vo.WeDailyVO;
 
 public interface WeDailyService {
@@ -20,5 +19,36 @@ public interface WeDailyService {
 	  
 	  // 찜한 영화 리스트 조회
 	  List<WeDailyVO> selectmove(WeDailyVO vo) throws Exception;
+	  
+	  // 설문조사 insert
+	  void insertSatisfaction(WeDailyVO vo) throws Exception;
+	  
+	  // 회원정보 수정
+	  boolean memberUpdate(WeDailyVO vo) throws Exception;
+	  
+	  // 게시판 등록
+	  void boardInsert(WeDailyVO vo) throws Exception;
+	  
+	  // 커뮤니티 조회
+	 List<WeDailyVO> selectBoard(WeDailyVO vo) throws Exception;
+
+	 // board update Page 조회
+	 List<WeDailyVO> boardUpdate_select(WeDailyVO vo) throws Exception;
+	 
+	 // 커뮤니티 update 변경
+	 void board_update(WeDailyVO vo) throws Exception;
+	 // 게시글 삭제
+	 void delete_board(WeDailyVO vo) throws Exception;
+	 
+	 // 게시판 검색 기능
+	 List<WeDailyVO> search(WeDailyVO vo) throws Exception;
+	 // 찜한 영화 조회
+	 List<WeDailyVO> select_movieLike(WeDailyVO vo) throws Exception;
+	 
+	 
+	 
+	 
+	
+	  
 	  
 }
